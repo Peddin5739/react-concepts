8108dce68f93872102fe3b907ec5105b53ceb812
Naveen Kumar Peddi <peddin5739@uhcl.edu>
2023-10-17 09:46:28 -0500

    SpreadAndRest.jsx component added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import SpreadAndRest from "./SpreadAndRest";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Spread and rest operator</h1>
+      <SpreadAndRest />
+    </div>
+  );
+}
+
+export default App;
